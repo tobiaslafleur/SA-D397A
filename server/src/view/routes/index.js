f3b54bca55af45eebf48983ec6584b6afacf0def
lucasstory <62547308+lucasstory@users.noreply.github.com>
2022-05-02 15:09:13 +0200

    Added types 2

--- server/src/view/routes/index.js
+++ server/src/view/routes/index.js
@@ -1,20 +1,14 @@
 import express from 'express'
 
 import user from './user.js'
-<<<<<<< Updated upstream
 import auth from './auth.js'
-=======
 import types from './types.js'
->>>>>>> Stashed changes
 
 const router = express.Router()
 
 router.use('/user', user) // localhost:3000/api/user/
 
-<<<<<<< Updated upstream
 router.use('/auth', auth) // localhost:3000/api/auth/
-=======
 router.use('/types', types) // localhost:3000/api/types/
->>>>>>> Stashed changes
 
 export default router
