756856bbb101e49a20785beaf6b43ad13499277e
unganemo <76483254+unganemo@users.noreply.github.com>
2022-05-16 14:39:28 +0200

    hope this doesnt fuck everything up

--- client/components/Messages.js
+++ client/components/Messages.js
@@ -3,13 +3,17 @@ import styles from '../styles/Messages.module.css'
 
 export default function Messages({messages}) {
     
+    const handleRead = async (message) => {
+        let url = `http://localhost:3000/api/message/${elem._id}`
+    }
+
     return (
         <div>
             {messages.map(elem => {
                 return(
                     <div className={styles.container}>
                         <p>{elem.text}</p>
-                        <button>Mark as read</button>
+                        <button onClick={() => handleRead(elem)}>Mark as read</button>
                     </div>
                 )
             })}
--- server/src/logic/message.logic.js
+++ server/src/logic/message.logic.js
@@ -5,11 +5,15 @@ import { getSubscribers } from './types.logic.js'
 const createNewMessage = async (payload) => {
     let typeSubs = {}
 
+    //Hämta alla subscribers av tillagd typ
     for(let i = 0; i < payload.types.length; i++) {
         typeSubs = await getSubscribers(payload.types[i])
     }
 
+    //Loopa igenom alla subscribers
     for(let i = 0; i < typeSubs.subscribers.length; i++) {
+
+        //Skapa objekt med subscriber, messageRead, text samt produktid
         const body = {
             userId: typeSubs.subscribers[i],
             messageRead: false,
@@ -17,6 +21,7 @@ const createNewMessage = async (payload) => {
             productId: payload._id
         }
 
+        //Skapa meddelande
         await createMessage(body)
     }
 }
